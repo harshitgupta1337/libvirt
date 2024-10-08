2e9d8e1b9cb8771c3e1f97830f173fc2509cd93a
Praveen K Paladugu <prapal@linux.microsoft.com>
2024-10-08 22:59:04 +0000

    ch: prepare domain definition for pci passthrough
    Check if the domain definition is valid for PCI passthrough and update
    it if necessary.
    
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

--- src/ch/ch_hostdev.c
+++ src/ch/ch_hostdev.c
@@ -26,3 +26,70 @@
 #define VIR_FROM_THIS VIR_FROM_CH
 
 VIR_LOG_INIT("ch.ch_hostdev");
+
+static int
+virCHDomainPrepareHostdevPCI(virDomainHostdevDef *hostdev)
+{
+    bool supportsPassthroughVFIO = virHostdevHostSupportsPassthroughVFIO();
+    virDeviceHostdevPCIDriverName *driverName =
+                                    &hostdev->source.subsys.u.pci.driver.name;
+
+    /* assign defaults for hostdev passthrough */
+    switch (*driverName) {
+    case VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_DEFAULT:
+        if (supportsPassthroughVFIO) {
+            *driverName = VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_VFIO;
+        } else {
+            virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                           _("host doesn't support passthrough of host PCI devices"));
+            return -1;
+        }
+        break;
+
+    case VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_VFIO:
+        if (!supportsPassthroughVFIO) {
+            virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                           _("host doesn't support VFIO PCI passthrough"));
+            return false;
+        }
+        break;
+
+    case VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_KVM:
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                       _("host doesn't support legacy PCI passthrough"));
+        return false;
+
+    case VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_XEN:
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                       _("CH does not support device assignment mode '%1$s'"),
+                       virDeviceHostdevPCIDriverNameTypeToString(*driverName));
+        return false;
+
+    default:
+    case VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_LAST:
+        virReportEnumRangeError(virDeviceHostdevPCIDriverName, *driverName);
+        break;
+    }
+
+    return true;
+}
+
+int
+virCHDomainPrepareHostdev(virDomainHostdevDef *hostdev)
+{
+    if (hostdev->mode != VIR_DOMAIN_HOSTDEV_MODE_SUBSYS)
+        return 0;
+
+    switch (hostdev->source.subsys.type) {
+    case VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_PCI:
+        return virCHDomainPrepareHostdevPCI(hostdev);
+    case VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_SCSI:
+    case VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_USB:
+    case VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_SCSI_HOST:
+    case VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_MDEV:
+    case VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_LAST:
+        break;
+    }
+
+    return 0;
+}
